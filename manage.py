d0ad873dae9be9aee4a4c13934a8f192fa35e3b4
Aijcoder <164013011+Aijcoder@users.noreply.github.com>
2024-11-04 14:19:56 +0000

    added citer

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_AutoCitationGenerator.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+#pip3 install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org django
